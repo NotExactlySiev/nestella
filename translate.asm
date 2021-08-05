55604bc24d1d3a7050719db252978df4e20b848d
NotExactlySiev <sadrabp@outlook.com>
2021-08-05 14:16:43 +0430

    emit interrupt subroutine created

--- translate.asm
+++ translate.asm
@@ -1,4 +1,4 @@
-TMemoryAccess: subroutine
+MirrorAddr: subroutine
 	lda AddrHi
         and #$10
         beq .ncart
@@ -35,7 +35,90 @@ TMemoryAccess: subroutine
         ora #$8
         sta NESAddrLo
 .nwrite
-	
 .mirrordone
+	rts
+
+
+TMemoryAccess: subroutine
+	jsr MirrorAddr
+        ; check if causes interrupt
 
         rts
+
+TJump: subroutine
+	
+	jmp EmitInterrupt
+
+TConditional: subroutine
+	lda #2
+        sta NESInstSize ; these are used here as parameters for the interrupt emitter
+	lda OpCode
+        lsr
+        lsr
+        lsr
+        lsr
+        lsr
+        ora #$8
+        sta NESOpCode
+        lda AddrLo
+	sta NESAddrLo
+
+EmitInterrupt: subroutine
+	ldy #0
+        
+        lda #INS_PHA
+        sta (TCachePtr),y
+        iny
+        lda #INS_PHP
+        sta (TCachePtr),y
+        iny
+        
+        dec NESInstSize
+        bmi .parsdone
+        
+        ldx #0
+.writepars
+
+        lda #INS_LDA_IMM
+        sta (TCachePtr),y
+        iny
+        
+	lda NESAddrLo,x
+        sta (TCachePtr),y
+        iny
+        
+        lda #INS_STA_ZPG
+        sta (TCachePtr),y
+        iny
+        
+        lda #var0
+        sta (TCachePtr),y
+        iny
+        
+        inx
+        cpx NESInstSize
+        bcc .writepars      
+.parsdone
+
+	lda #INS_LDA_IMM
+        sta (TCachePtr),y
+        iny
+        
+        lda NESOpCode
+        sta (TCachePtr),y
+        iny
+        
+        lda #INS_JMP_ABS
+        sta (TCachePtr),y
+        iny
+        
+        lda #<InterruptHandler
+        sta (TCachePtr),y
+        iny
+        
+        lda #>InterruptHandler
+        sta (TCachePtr),y
+        
+        jmp TranslationDone
+        
+        
